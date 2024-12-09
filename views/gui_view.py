87dd9c86f70364816a9e5ed36069637c359cae16
Dominik Gasowski <dominiggk@gmail.com>
2024-12-09 18:28:19 +0100

    guiv2

--- views/gui_view.py
+++ views/gui_view.py
@@ -1,4 +1,6 @@
 import pygame
+from models import CustomBoard, MenuOption, HistoryOption
+from typing import List, Union, Tuple
 
 class GuiView:
     def __init__(self):
@@ -13,23 +15,60 @@ class GuiView:
         self.button_font = pygame.font.Font(None, 36)
         self.text_list = ["Play with bot", "Play multiplayer", "Display history", "Leave game"]
 
-        self.button_bot = pygame.Rect((self.SCREEN_WIDTH-250)/2,250,250,50) # (x_start_od_lewej,y_start_od_gory,szerokosc,wysokosc)
-        self.button_multiplayer = pygame.Rect((self.SCREEN_WIDTH-250)/2, 350, 250, 50)  # (x_start_od_lewej,y_start_od_gory,szerokosc,wysokosc)
-        self.button_history = pygame.Rect((self.SCREEN_WIDTH-250)/2, 450, 250, 50)  # (x_start_od_lewej,y_start_od_gory,szerokosc,wysokosc)
-        self.button_leave_game = pygame.Rect((self.SCREEN_WIDTH-250)/2, 550, 250, 50)  # (x_start_od_lewej,y_start_od_gory,szerokosc,wysokosc)
+        self.button_bot = pygame.Rect((self.SCREEN_WIDTH-250)/2,270,250,50) # (x_start_od_lewej,y_start_od_gory,szerokosc,wysokosc)
+        self.button_multiplayer = pygame.Rect((self.SCREEN_WIDTH-250)/2, 340, 250, 50)  # (x_start_od_lewej,y_start_od_gory,szerokosc,wysokosc)
+        self.button_history = pygame.Rect((self.SCREEN_WIDTH-250)/2, 410, 250, 50)  # (x_start_od_lewej,y_start_od_gory,szerokosc,wysokosc)
+        self.button_leave_game = pygame.Rect((self.SCREEN_WIDTH-250)/2, 480, 250, 50)  # (x_start_od_lewej,y_start_od_gory,szerokosc,wysokosc)
         self.button_list = [self.button_bot, self.button_multiplayer, self.button_history, self.button_leave_game]
 
         self.button_color = (255,255,255)
+
+        self.pieces_menu_path = "C:\\Users\\user\\Desktop\\chess\\chess\\images\\menu_pieces.png"
+        self.chess_string_menu_path = "C:\\Users\\user\\Desktop\\chess\\chess\\images\\chess_string_menu.png"
+
     def display_menu(self):
+        current_cursor = pygame.SYSTEM_CURSOR_ARROW
+
         while self.running:
+            self.screen.fill((61, 61, 59))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-            self.screen.fill((61, 61, 59))
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        mouse_pos = pygame.mouse.get_pos()
+                        if self.button_history.collidepoint(mouse_pos):
+                            return MenuOption.DISPLAY_HISTORY
+                        elif self.button_leave_game.collidepoint(mouse_pos):
+                            pygame.quit()
+                            #return MenuOption.LEAVE_THE_GAME
+                        elif self.button_bot.collidepoint(mouse_pos):
+                            return MenuOption.PLAY_WITH_BOT
+                        elif self.button_multiplayer.collidepoint(mouse_pos):
+                            return MenuOption.PLAY_MULTIPLAYER
 
+            # render buttons and texts on
             for button,text in zip(self.button_list,self.text_list):
                 pygame.draw.rect(self.screen, self.button_color, button, border_radius=20)
                 self.render_button_text(button, text)
+
+            # mouse change on buttons
+            mouse_pos = pygame.mouse.get_pos()
+            for button in self.button_list:
+                if button.collidepoint(mouse_pos):
+                    self.render_border_image(button)
+                    if current_cursor != pygame.SYSTEM_CURSOR_HAND:
+                        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+                        current_cursor = pygame.SYSTEM_CURSOR_HAND
+                    break
+            else:
+                if current_cursor != pygame.SYSTEM_CURSOR_ARROW:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+                    current_cursor = pygame.SYSTEM_CURSOR_ARROW
+
+            # render images
+            self.render_image(self.pieces_menu_path,55,600)
+            self.render_image(self.chess_string_menu_path, 280,20)
 
             pygame.display.update()
         pygame.quit()
@@ -38,3 +77,19 @@ class GuiView:
         text_surface = self.button_font.render(text, True, self.button_text_color)
         text_rect = text_surface.get_rect(center=button_rect.center)
         self.screen.blit(text_surface,text_rect)
+
+    def render_border_image(self, button_rect):
+        transparent_surface = pygame.Surface((button_rect.width, button_rect.height), pygame.SRCALPHA)
+        transparent_surface.fill((0, 0, 0, 0))
+        self.screen.blit(transparent_surface, button_rect.topleft)
+        pygame.draw.rect(self.screen, (0,0,0), button_rect, 4, border_radius=20)
+
+    def render_image(self, image_path, x, y):
+        image = pygame.image.load(image_path)
+        self.screen.blit(image, (x,y))
+
+    def display_board(self, board: CustomBoard, flip: bool=False, play_sound: bool = False) -> None:
+        pass
+
+    def display_history(self, bot_files: List[str], multiplayer_files: List[str]) -> Tuple[HistoryOption, Union[str, None], int]:
+        pass
